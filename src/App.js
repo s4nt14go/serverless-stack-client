d7e456fda25f9d911cea60bdb50b01a79260bc88
s4nt14go <santiagopergamo@gmail.com>
2020-10-05 10:21:01 -0300

    Adding Links in the Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
@@ -14,6 +15,16 @@ function App() {
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <LinkContainer to="/signup">
+              <NavItem>Signup</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
