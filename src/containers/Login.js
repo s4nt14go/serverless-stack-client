5b41e607d07b980856c14a23005c835df97efbb7
s4nt14go <santiagopergamo@gmail.com>
2020-10-05 10:38:37 -0300

    Login with AWS Cognito

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import "./Login.css";
+import { Auth } from "aws-amplify";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -10,8 +11,15 @@ export default function Login() {
     return email.length > 0 && password.length > 0;
   }
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+
+    try {
+      await Auth.signIn(email, password);
+      alert("Logged in");
+    } catch (e) {
+      alert(e.message);
+    }
   }
 
   return (
