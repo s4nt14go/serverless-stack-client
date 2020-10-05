62ff98dfe146e773b739ff51d3abfbb88efbeffd
s4nt14go <santiagopergamo@gmail.com>
2020-10-05 11:46:43 -0300

    Create a Custom React Hook to Handle Form Fields

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -6,17 +6,20 @@ import { useAppContext } from "../libs/contextLib";
 import { useHistory } from "react-router-dom";
 import LoaderButton from "../components/LoaderButton";
 import {onError} from "../libs/errorLib";
+import { useFormFields } from "../libs/hooksLib";
 
 export default function Login() {
   const history = useHistory();
   const { userHasAuthenticated } = useAppContext();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: ""
+  });
   const [isLoading, setIsLoading] = useState(false);
 
   function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return fields.email.length > 0 && fields.password.length > 0;
   }
 
   async function handleSubmit(event) {
@@ -25,7 +28,7 @@ export default function Login() {
     setIsLoading(true);
 
     try {
-      await Auth.signIn(email, password);
+      await Auth.signIn(fields.email, fields.password);
       userHasAuthenticated(true);
       history.push("/");
     } catch (e) {
@@ -42,15 +45,15 @@ export default function Login() {
           <FormControl
             autoFocus
             type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
+            value={fields.email}
+            onChange={handleFieldChange}
           />
         </FormGroup>
         <FormGroup controlId="password" bsSize="large">
           <ControlLabel>Password</ControlLabel>
           <FormControl
-            value={password}
-            onChange={e => setPassword(e.target.value)}
+            value={fields.password}
+            onChange={handleFieldChange}
             type="password"
           />
         </FormGroup>
